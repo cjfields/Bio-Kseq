08655171a56b20c7755d91c06214813e5db06505
Chris Fields <cjfields@bioperl.org>
2012-03-02 15:49:08 -0600

    c&p braino

--- lib/Bio/Readfq.xs
+++ lib/Bio/Readfq.xs
@@ -15,7 +15,7 @@ KSEQ_INIT(gzFile, gzRead)
 typedef kseq_t*         Bio__Readfq__Iterator;
 typedef gzFile          Bio__Readfq__File;
 
-MODULE = Bio::Readfq PACKAGE = Bio::Readfq::File  PREFIX=readfq_
+MODULE = Bio::Readfq PACKAGE = Bio::Readfq  PREFIX=readfq_
 
 Bio::Readfq::File
 readfq_open(filename, mode="r")
@@ -33,6 +33,21 @@ readfq_DESTROY(fp)
     PROTOTYPE: $
     CODE:
         gzclose(fp);
+
+Bio::Readfq::Iterator
+file_iterator(fp)
+    Bio::Readfq::File fp
+    PROTOTYPE: $
+    CODE:
+        RETVAL = kseq_init(fp);
+
+## Bio::Readfq::Iterator
+## readfq_iterator(fp)
+## kseq_init(fp)
+
+MODULE = Bio::Readfq PACKAGE = Bio::Readfq::File   PREFIX=file_
+
+
 
 MODULE = Bio::Readfq PACKAGE = Bio::Readfq::Iterator   PREFIX=it_
 
--- lib/Bio/readfq.pm
+++ lib/Bio/readfq.pm
@@ -1,6 +1,41 @@
-use Moose;
-package Bio::readfq;
-
+package Bio::Readfq;
+use strict;
+use warnings;
 # ABSTRACT: low-level XS-based FASTQ/FASTA parser
+
+use base qw(DynaLoader);
+bootstrap Bio::Readfq;
+
+sub new {
+    my ($class, %args) = @_;
+    if (ref $class) {
+        $class = ref $class;
+    }
+    my $self = bless \%args, $class;
+    $self->_init();
+    $self;
+}
+
+sub file_name {
+    my $self = shift;
+    $self->{file}
+}
+
+# convenience method
+sub next_seq {
+    my $self = shift;
+    return $self->{_it}->next_seq();
+}
+
+sub _init {
+    my $self = shift;
+    $self->{_fp} = $self->open($self->file_name);
+    $self->{_it} = $self->init_iterator($self->{_fp});
+}
+
+sub _fp {
+    my $self = shift;
+    $self->{_fp}
+}
 
 1;
