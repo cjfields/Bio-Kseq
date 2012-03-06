5f2eb8afef8fe9bb1235b224ed39146013c06717
Chris Fields <cjfields@bioperl.org>
2012-03-06 16:58:24 -0600

    add stub dist and test files

---
+++ lib/Bio/Klib.pm
@@ -0,0 +1,10 @@
+package Bio::Klib;
+
+# ABSTRACT: Perl XS interface to klib functions
+
+# VERSION
+
+1;
+
+__END__
+
---
+++ t/kseq.t
@@ -0,0 +1,6 @@
+use Test::Most;
+use lib qw(.. ../blib/lib ../blib/arch);
+use Bio::Kseq;
+use inc::TestHelper;
+
+
